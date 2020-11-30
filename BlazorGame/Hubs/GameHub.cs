f47e6d3ccd31754e9bfb07dd65035a2d9834a817
Darren Neimke <darren.neimke@gmail.com>
2020-12-01 05:39:50 +1030

    tidy ups

--- BlazorGame/Hubs/GameHub.cs
+++ BlazorGame/Hubs/GameHub.cs
@@ -1,18 +1,9 @@
-﻿using BlazorGame.Data;
-using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.SignalR;
 
 namespace BlazorGame.Hubs
 {
     public class GameHub : Hub
     {
-        readonly GameSessionService _gameService;
-
-        public GameHub(GameSessionService sessionService) => _gameService = sessionService;
-
-        public async Task CreateGame(string userName, int pinCode) => await _gameService.CreateGame(Context.ConnectionId, userName, pinCode);
-        public async Task JoinGame(string userName, Guid gameId, int pinCode) => await _gameService.JoinGame(Context.ConnectionId, userName, gameId, pinCode);
-        public async Task LeaveGame(Guid gameId, int pinCode) => await _gameService.LeaveGame(Context.ConnectionId, gameId, pinCode);
+        
     }
 }
